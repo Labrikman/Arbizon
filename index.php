975f3c881c8865683c59ce8b86a87eadfa11c8cd
labrikman <labrikman@hotmail.fr>
2019-05-17 23:41:10 +0200

    day1 - 1h

--- index.php
+++ index.php
@@ -12,19 +12,66 @@
 </head>
 <body>
     <header>
-        <div class="logo"></div>
-        <nav>
-            <ul>
-                <li></li>
-                <li></li>
-            </ul>
-        </nav>
+        <section class="wrapper">
+            <div class="logo"></div>
+            <nav class="nav">
+                <ul>
+                    <li>Home</li>
+                    <li>Hikes</li>
+                </ul>
+            </nav>
+            <h5 class="header-sub-title">
+                <strong>Mountains worthy of Tolkien's stories</strong>
+            </h5>
+            <h1 class="header-title">Discover<br>Arbizon.</h1>       
+            <a href="" class="header-link">Interested? Let’s go!</a>
+            <div class="header-line"></div>
+        </section>
     </header>
     <main>
         <section class="map">
-
+            <p class="sub-title"><strong>The Adour of Payolle is born on its northern slope.</strong></p>
+            <h3 class="main-title">
+                The most beautiful view <br> 
+                of the Pyrénées.
+            </h3>
+            <p>
+                The Arbizon (2,831 m) is the highest point of the Arbizon massif in the Hautes-Pyrénées. It is located east of the Néouvielle massif, between the Aure valley to the east, and the Campan valley to the north.
+            </p> 
+            <p>   
+                From a geological point of view, the peak of the Arbizon lies at the contact between the granitic massif of Neouvielle (pluton) and the various shales and marls of the Carboniferous.
+            </p> 
+            <p>  
+                Thus, it is located in the halo of metamorphism caused by the rise of magma within the continental lithosphere, during the various movements of orogenesis that the region has known. Its geological wealth is known to the greatest number: one can find there particular minerals, resulting from the partial recrystallization of the schistose country, such as garnets, axinite, vesuvianite, etc.
+            </p>
         </section>
+
         <figure class="mount"></figure>
+
+        <section class="article wrapper">
+            <article>
+                <img class="img-article" src="img/desktop_section2_image.jpg" alt="montage-pyrénées">
+                <div class="description">
+                    <p class="sub-title"><strong>A hard hike</strong></p>
+                    <h3>The raptor ridge</h3>
+                    <p>The normal route to the Arbizon summit is on the south side, the Aulon Valley. The starting point is the barns of Lurgues, near the village of Aulon, about 1400 m above sea level.</p>
+                    <p>Another way of access exists: it takes the valley of the lake of Arou and passes within the Arbizon-Montfaucon circus to climb on the crest by the breach of Aurey…</p>
+                    <a href="" class="link">Discover this hike !</a>
+                    <div class="line"></div>
+                </div>
+            </article>
+        </section>
+        <section class="article wrapper">
+            <div class="description">
+                <p class="sub-title"><strong>A hard hike</strong></p>
+                <h3>The raptor ridge</h3>
+            </div>
+            <div class="all-article">
+                <?php 
+                echo "coucou";
+                ?> 
+            </div>
+        </section>
     </main>
 
     <footer>
